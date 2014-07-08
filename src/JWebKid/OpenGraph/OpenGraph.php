8180db00b65a86022e3bb29535144edec614f46a
Luca Tabone <jwebkid@users.noreply.github.com>
2014-07-08 10:40:31 +0200

    Comments
    Added comments

--- src/JWebKid/OpenGraph/OpenGraph.php
+++ src/JWebKid/OpenGraph/OpenGraph.php
@@ -1,16 +1,38 @@
 <?php namespace JWebKid\OpenGraph;
 
 class OpenGraph{
+	/**
+	* Open Graph configuration.
+	*
+	* @var array
+	*/
 	private $_config;
 
+	/**
+	 * Create a new Open Graph Object
+	 * 
+	 * @param array $config
+	 * @return void
+	 */
 	public function __construct($config){
 		$this->_config = $config;
 	}
-
+	
+	/**
+	 * Display Open Graph Tags
+	 * 
+	 * @return string 
+	 */
 	public function view(){
 		$this->traverse($this->_config);
 	}
 
+	/**
+	 * Parse and build the Open Graph Meta Tags
+	 * 
+	 * @param array $config
+	 * @param string $path
+	 */
 	private function traverse($config, $path = "og"){
 		if(count($config)){
 			foreach($config as $key => $value){
